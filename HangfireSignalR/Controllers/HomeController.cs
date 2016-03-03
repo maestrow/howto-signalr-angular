0c3e70d38426e4a7d9f91679d465418b5623bbf7
maestrow <andrey.shmelev@gmail.com>
2016-03-04 00:21:30 +0300

    Добавил страничку с полем ввода и кнопкой submit. Пользователь вводит имя задачи, в action-методе имя задачи выводится в Debug.

---
+++ HangfireSignalR/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HangfireSignalR.Models;
+
+namespace HangfireSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new TaskProperties());
+        }
+
+        public ActionResult StartTask(TaskProperties model)
+        {
+            Debug.WriteLine(model.TaskName);
+            return View("Index", model);
+        }
+    }
+}
---
+++ HangfireSignalR/Models/TaskProperties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HangfireSignalR.Models
+{
+    public class TaskProperties
+    {
+        public string TaskName { get; set; }
+    }
+}
