aa4fedc909dca8e46d09d2244cf734c92a768c58
maestrow <andrey.shmelev@gmail.com>
2016-03-04 01:30:28 +0300

    Подключил пакет SignalR и сделал hello world пример

---
+++ HangfireSignalR/SignalRHubs/TasksHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace HangfireSignalR.SignalRHubs
+{
+    public class TasksHub : Hub
+    {
+        public void Hello(string name)
+        {
+            Clients.All.hello(name);
+        }
+    }
+}
---
+++ HangfireSignalR/Tasks/SampleAsyncTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HangfireSignalR.Tasks
+{
+    public static class SampleAsyncTask
+    {
+        public static async Task StartCalculation(int timeDelay, CancellationToken token, IProgress<int> progress)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    if (progress != null)
+                        progress.Report(100);
+                    token.ThrowIfCancellationRequested();
+                }
+                if (progress != null)
+                    progress.Report(i);
+
+                await Task.Delay(timeDelay / 100);
+            }
+
+            if (progress != null)
+                progress.Report(100);
+        }
+    }
+}
