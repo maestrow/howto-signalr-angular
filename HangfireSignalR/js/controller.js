1a0ac2904c6ecf33649420c65b330f419ee757f9
maestrow <andrey.shmelev@gmail.com>
2016-03-04 12:33:33 +0300

    Клиентская часть использует angular

---
+++ HangfireSignalR/js/controller.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('app', []);
+
+app.controller('TaskListCtrl', function ($scope) {
+	$scope.tasks = [
+    {
+    	name: 'Nexus S',
+    	progress: 5
+    },
+    {
+    	name: 'bbbbb',
+    	progress: 75
+    },
+    {
+    	name: 'cccccc',
+    	progress: 35
+    }
+	];
+});
